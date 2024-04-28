9cad7f7fa9eb87e371eacaf78374a2b000e91311
Ganganthi <elithilu@hotmail.com>
2024-04-27 21:59:25 -0300

    feat: use powerlevel10k

--- kickstart.sh
+++ kickstart.sh
@@ -9,9 +9,11 @@ chsh -s $(which zsh)
 
 # Installing oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+rm ~/.zshrc
 
 # Oh-my-zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # Installing CLI tools
 sudo apt install -y tmux exa tree bat ripgrep fzf nodejs npm unzip \
