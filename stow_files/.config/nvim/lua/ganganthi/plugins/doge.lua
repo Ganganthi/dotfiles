19cdee5b0ade6a2d09856c7d2c33c252820a804c
Ganganthi <elithilu@hotmail.com>
2025-01-09 11:54:51 +0000

    refactor: disable doge

--- stow_files/.config/nvim/lua/ganganthi/plugins/doge.lua
+++ stow_files/.config/nvim/lua/ganganthi/plugins/doge.lua
@@ -1,8 +1,9 @@
-return {
-  "kkoomen/vim-doge",
-  build = ":call doge#install()",
-  config = function()
-    vim.g.doge_doc_standard_python = "google"
-    vim.g.doge_mapping = "<Leader>d"
-  end,
-}
+return {}
+-- return {
+--   "kkoomen/vim-doge",
+--   build = ":call doge#install()",
+--   config = function()
+--     vim.g.doge_doc_standard_python = "google"
+--     vim.g.doge_mapping = "<Leader>dg"
+--   end,
+-- }
