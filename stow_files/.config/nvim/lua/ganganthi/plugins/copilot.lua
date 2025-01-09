cbfb13e52d2e427e3669ae909db1e79d2a0c2630
Ganganthi <elithilu@hotmail.com>
2025-01-09 11:55:07 +0000

    refactor: disable copilot

--- stow_files/.config/nvim/lua/ganganthi/plugins/copilot.lua
+++ stow_files/.config/nvim/lua/ganganthi/plugins/copilot.lua
@@ -1,22 +1,23 @@
-return {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    config = function()
-        require("copilot").setup({
-            panel = {
-                enabled = false,
-            },
-            suggestion = {
-                auto_trigger = true,
-                keymap = {
-                    accept = "<M-l>",
-                    accept_word = "<M-m>",
-                    accept_line = false,
-                    next = "<M-]>",
-                    prev = "<M-[>",
-                    dismiss = "<C-]>",
-                },
-            },
-        })
-    end,
-}
+return {}
+-- return {
+--     "zbirenbaum/copilot.lua",
+--     event = "InsertEnter",
+--     config = function()
+--         require("copilot").setup({
+--             panel = {
+--                 enabled = false,
+--             },
+--             suggestion = {
+--                 auto_trigger = true,
+--                 keymap = {
+--                     accept = "<M-l>",
+--                     accept_word = "<M-m>",
+--                     accept_line = false,
+--                     next = "<M-]>",
+--                     prev = "<M-[>",
+--                     dismiss = "<C-]>",
+--                 },
+--             },
+--         })
+--     end,
+-- }
