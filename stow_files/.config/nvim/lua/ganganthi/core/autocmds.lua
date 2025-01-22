d43580e68b05762977fd4a9b05945c780be040dd
Ganganthi <elithilu@hotmail.com>
2025-01-22 11:15:16 +0000

    feat: add command to list linters active

--- stow_files/.config/nvim/lua/ganganthi/core/autocmds.lua
+++ stow_files/.config/nvim/lua/ganganthi/core/autocmds.lua
@@ -1,26 +1,38 @@
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-vim.api.nvim_create_autocmd('TextYankPost', {
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = '*',
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
 })
 
 -- Check if we need to reload the file when it changed
 vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-    group = vim.api.nvim_create_augroup("checktime", { clear = true }),
-    command = "checktime",
+  group = vim.api.nvim_create_augroup("checktime", { clear = true }),
+  command = "checktime",
 })
 
 -- resize splits if window got resized
 vim.api.nvim_create_autocmd({ "VimResized" }, {
-    group = vim.api.nvim_create_augroup("resize_splits", { clear = true }),
-    callback = function()
-        local current_tab = vim.fn.tabpagenr()
-        vim.cmd("tabdo wincmd =")
-        vim.cmd("tabnext " .. current_tab)
-    end,
+  group = vim.api.nvim_create_augroup("resize_splits", { clear = true }),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
 })
+
+-- Show linters for the current buffer's file type
+vim.api.nvim_create_user_command("LintInfo", function()
+  local filetype = vim.bo.filetype
+  local linters = require("lint").linters_by_ft[filetype]
+
+  if linters then
+    print("Linters for " .. filetype .. ": " .. table.concat(linters, ", "))
+  else
+    print("No linters configured for filetype: " .. filetype)
+  end
+end, {})
--- stow_files/.config/nvim/lua/ganganthi/core/init.lua
+++ stow_files/.config/nvim/lua/ganganthi/core/init.lua
@@ -1,2 +1,3 @@
 require("ganganthi.core.keymaps")
 require("ganganthi.core.options")
+require("ganganthi.core.autocmds")
