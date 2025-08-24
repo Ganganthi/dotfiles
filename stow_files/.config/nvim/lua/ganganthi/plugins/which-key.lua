91ac2caede5616f03c3f7c83d0864923173ec254
Ganganthi <elithilu@hotmail.com>
2025-08-24 15:58:25 -0300

    feat: add some keymap descriptions

--- stow_files/.config/nvim/lua/ganganthi/plugins/which-key.lua
+++ stow_files/.config/nvim/lua/ganganthi/plugins/which-key.lua
@@ -1,9 +1,21 @@
 return {
-    'folke/which-key.nvim',
-    event = "VeryLazy",
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 500
-    end,
-    opts = {}
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 500
+  end,
+  opts = {
+    -- delay between pressing a key and opening which-key (milliseconds)
+    -- this setting is independent of vim.o.timeoutlen
+    delay = 0,
+
+    -- Document existing key chains
+    spec = {
+      { "<leader>s", group = "[S]earch" },
+      { "<leader>f", group = "[F]ind" },
+      -- { "<leader>t", group = "[T]oggle" },
+      -- { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+    },
+  },
 }
